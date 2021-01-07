10ffe5bde8f46fde45fa042a6f5f91da61271682
azaelsanchez <cem20619@gmail.com>
2021-01-07 04:17:18 +0100

    app.js con express

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 'use strict'
 
 var mongoose = require('mongoose');
+var app = require('./app');
+var port = process.env.PORT || 3977;
 
 mongoose.connect('mongodb://localhost:27017/Appmusical', (err, res)=> {
     if(err){
         throw err;
     }else{
         console.log("Conexión correcta con la base de datos");
+
+        app.listen(port, function(){
+           console.log("Conectado al servidor "+port+" Appmusical");
+        });
     }
 });
