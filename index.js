df22ce0e79aee5574f8e7460cc309078234af358
azaelsanchez <cem20619@gmail.com>
2021-01-07 04:34:45 +0100

    Creacion de models

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var mongoose = require('mongoose');
 var app = require('./app');
 var port = process.env.PORT || 3977;
 
+mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/Appmusical', (err, res)=> {
     if(err){
         throw err;
